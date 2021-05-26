2b939880afeab3f7461f31c76646b9e794b4b5a4
Andrii Kotlenko <andrii.kotlenko@gmail.com>
2021-05-26 12:32:35 +0300

    5

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,8 @@ const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
 const closeEl = document.querySelector(".menu__close");
 const overlayEl = document.querySelector(".menu__overlay");
+const progresses = document.querySelectorAll(".skills__progress-range"),
+    progressBars = document.querySelectorAll(".skills__progress-bar-range");
 
 const closeArr = [];
 closeArr.push(closeEl, overlayEl);
@@ -13,3 +15,7 @@ hamburger.addEventListener("click", () => {
 closeArr.forEach(item => {
     item.addEventListener("click", () => menu.classList.remove("active"));
 });
+
+progressBars.forEach(
+    (item, index) => (item.style.width = progresses[index].innerText)
+);
